53a450f91a5a661dfcfed3ac488a1f16379f71da
joabm <84096469+joabm@users.noreply.github.com>
2022-07-10 15:20:23 -0700

    Initial Commit

---
+++ Meme 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meme 1.0
+//
+//  Created by Joab Maldonado on 7/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
