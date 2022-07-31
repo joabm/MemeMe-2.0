178a549a8d5d6656d50dbd7825cc0001a05fea63
joabm <84096469+joabm@users.noreply.github.com>
2022-07-30 17:10:00 -0700

    added MemeCollectionViewCell and code

---
+++ Meme 1.0/MemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewCell.swift
+//  Meme 1.0
+//
+//  Created by Joab Maldonado on 7/30/22.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memedImageVeiw:UIImageView!
+}
