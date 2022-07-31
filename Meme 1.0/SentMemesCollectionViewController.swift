8275fdcae43003064b4f1ee0ce1c229c8896d9e4
joabm <84096469+joabm@users.noreply.github.com>
2022-07-30 17:09:13 -0700

    added SentMemesCollectionViewController file and code

---
+++ Meme 1.0/SentMemesCollectionViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SentMemesCollectionViewController.swift
+//  Meme 1.0
+//
+//  Created by Joab Maldonado on 7/30/22.
+//
+
+import Foundation
+import UIKit
+
+class SentMemesCollectionViewController: UICollectionViewController {
+    
+    // MARK: Properties
+    
+    //Access the memes array in AppDelegate
+    var memes: [Meme]! {
+        let object = UIApplication.shared.delegate
+        let appDelegate = object as! AppDelegate
+        return appDelegate.memes
+    }
+    
+    // MARK: Life Cycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    // MARK: Collection View Data Source
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionViewCell", for: indexPath) as! MemeCollectionViewCell
+        let meme = self.memes[(indexPath as NSIndexPath).row]
+        
+        // Set the image
+        cell.memedImageVeiw?.image = meme.memedImage
+        
+        return cell
+    }
+    
+    /*override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+        
+        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "VillainDetailViewController") as! VillainDetailViewController
+        detailController.villain = self.allVillains[(indexPath as NSIndexPath).row]
+        self.navigationController!.pushViewController(detailController, animated: true)
+        
+    }*/
+    
+}
